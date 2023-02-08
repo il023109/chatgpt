f6c99ecc9c18ea1cd6d0ad25cb8f65508bef993a
il023109 <il023109@users.noreply.github.com>
2023-02-07 21:35:19 -0600

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -32,7 +32,7 @@ export default async function (req, res) {
       prompt: animal,
       temperature: 0.6,
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    res.status(200).json({ result: completion.data.choices[1].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
