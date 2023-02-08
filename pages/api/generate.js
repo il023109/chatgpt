0a6d9f0d97f0cbb3b2e0aa405d6ba9841335da88
il023109 <il023109@users.noreply.github.com>
2023-02-07 21:39:25 -0600

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -26,27 +26,45 @@ export default async function (req, res) {
     return;
   }
 
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: animal,
-      temperature: 0.6,
-    });
-    res.status(200).json({ result: completion.data.choices[0].text + "\n" + completion.data.choices[1].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
+ try {
+    const res = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt: "It was the best of times",
+        max_tokens: 100,
+        temperature: 0,
+        stream: true,
+    }, { responseType: 'stream' });
+    
+    res.data.on('data', data => {
+        const lines = data.toString().split('\n').filter(line => line.trim() !== '');
+        for (const line of lines) {
+            const message = line.replace(/^data: /, '');
+            if (message === '[DONE]') {
+                return; // Stream finished
+            }
+            try {
+                const parsed = JSON.parse(message);
+                console.log(parsed.choices[0].text);
+            } catch(error) {
+                console.error('Could not JSON parse stream message', message, error);
+            }
         }
-      });
+    });
+} catch (error) {
+    if (error.response?.status) {
+        console.error(error.response.status, error.message);
+        error.response.data.on('data', data => {
+            const message = data.toString();
+            try {
+                const parsed = JSON.parse(message);
+                console.error('An error occurred during OpenAI request: ', parsed);
+            } catch(error) {
+                console.error('An error occurred during OpenAI request: ', message);
+            }
+        });
+    } else {
+        console.error('An error occurred during OpenAI request', error);
     }
-  }
 }
 
 function generatePrompt(animal) {
